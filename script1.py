72080a07e5333124f2d440ac763a09d0bbe38906
DjzCAF <amalo.contact@gmail.com>
2017-05-24 10:12:56 +1100

    Création d'un script 1 et d'un script 2, test de l'import entre deux fichiers en python

---
+++ script1.py
@@ -0,0 +1,11 @@
+"""
+suite
+"""
+
+def suite():
+    min = 0
+    for i in range(1,11):
+        print(min)
+        min = i+min
+
+suite()
---
+++ script2.py
@@ -0,0 +1,9 @@
+"""
+Script 2
+"""
+
+print("--Debut script1--")
+import script1
+print("--Fin script2--")
+print("script2")
+
