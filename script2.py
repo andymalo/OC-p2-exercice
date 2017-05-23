1ee73e1814e4e1dfe4f9a551bc44a3501db46e4f
DjzCAF <amalo.contact@gmail.com>
2017-05-24 10:19:08 +1100

    Rajout d'un 3e script liant les 2 premiers scripts

--- script2.py
+++ script2.py
@@ -4,8 +4,8 @@ Script 2
 
 print("--Debut script1--")
 import script1
-print("--Fin script2--")
-print("script2")
-
+print("--Fin script1--")
+print("--Debut script2--")
 for i in range(1,5):
     print(i)
+print("--Fin script2--")
---
+++ script3.py
@@ -0,0 +1,7 @@
+print("---script1")
+import script1
+
+print("---script2")
+import script2
+
+print("Fin des script1 et script2")
